503c9ca171c0e88d74b7312ee6aaa39cd12aeec7
Saranyajayakumaran jayakumaran <jsaran.bj@gmail.com>
2024-05-21 11:15:05 +0000

    install summernote app to enchance the admin panel heading

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display=('title','slug','status')
+    search_field=['title']
+    last_filter=('status',)
+    prepopulate_fields={'slug':('title',)}
+    summernote_fields=('content',)
+
 
 # Register your models here.
-admin.site.register(Post)
 admin.site.register(Comment)
