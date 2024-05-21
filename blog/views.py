3e3e53939b98f63e3ed0fc281c251c9bc6d1c6c5
Saranyajayakumaran jayakumaran <jsaran.bj@gmail.com>
2024-05-21 12:52:01 +0000

    Add base.html and index.html in tmplates directory

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,8 @@ from .models import Post
 # Create your views here.
 
 class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=2)
-    template_name = "post_list.html"
-    #model=Post
+    model=Post
+    queryset = Post.objects.filter(status=1)
+    template_name="blog/index.html"
+
+    
